7ed6836ae239308a4acca073085c5b09d2e8fb5b
ranjithpothem <88814205+ranjithpothem@users.noreply.github.com>
2025-05-20 06:56:02 +0000

    Changes to be committed: 	new file:   FuncationalInterfaces/Predi.class 	new file:   FuncationalInterfaces/PredicateExaple.class 	modified:   FuncationalInterfaces/PredicateExaple.java

--- FuncationalInterfaces/PredicateExaple.java
+++ FuncationalInterfaces/PredicateExaple.java
@@ -6,8 +6,10 @@ import java.util.function.Predicate;
 class PredicateExaple {
     public static void main(String[] args) {
         Predicate<Integer> predicate=num-> num>10;
+        Predicate<String> predicateString=str-> str.contains("ji");
         
         System.out.println(predicate.test(11));
+         System.out.println(predicateString.test("ranjith"));
        Predi test1=num -> num<10;
         System.out.println(test1.test1(9));
     }
